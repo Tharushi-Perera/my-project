dc91b6c6e538113a9a8df76a2f6c2ed93cc19fca
Tharushi Perera <tharushiperera991@gmail.com>
2025-03-23 20:01:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from Basic operations import add, subtract, multiply
+from Greetingss import greet_user
+
+print(greet_user("Tharushi"))
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+
