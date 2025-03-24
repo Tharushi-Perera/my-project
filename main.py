1de1c529c79be6386044f174c2040349bebcfbad
Tharushi Perera <tharushiperera991@gmail.com>
2025-03-24 07:24:25 +0530

    Added divide function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from Basic operations import add, subtract, multiply
+from Basic operations import add, subtract, multiply, divide
 from Greetings import greet_user
 
 print(greet_user("Tharushi"))
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 5))
 
